a81d8ec0f6f80fc04d5dc23ef934602d76ec4930
Kaan Çembertaş <kaan@thekompanion.com>
2020-12-08 00:21:54 +0300

    Bugfixes

--- main.c
+++ main.c
@@ -10,7 +10,6 @@ char *read_input(char *path);
 int search_katar(char *str, char *katar);
 char *substring(char *str, int start_index, int length);
 void partition_input(char *input, char *katar, char *local_input, int p, int my_rank);
-void Show_local_inputs(int p, int my_rank,char *local_input);
 
 int main(int argc, char *argv[])
 {
@@ -26,6 +25,7 @@ int main(int argc, char *argv[])
     char *local_input;
     int size_per_proc;
     int partition_size;
+    int katar_index;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
@@ -42,7 +42,6 @@ int main(int argc, char *argv[])
 
     local_input = (char *) malloc(sizeof(char)*partition_size);
     partition_input(input,katar,local_input,p,my_rank);
-    Show_local_inputs(p,my_rank,local_input);
     local_katar_location = search_katar(local_input,katar);
 
     MPI_Gather(&local_katar_location,1,MPI_INT,
@@ -51,9 +50,18 @@ int main(int argc, char *argv[])
 
     if(my_rank == 0)
     {
+        printf("==================\n");
         for(i=0;i<p;i++)
         {
-            printf("%d\n",katar_locations[i]);
+            if(katar_locations[i] == -1)
+            {
+                printf("Process %d: Cannot find the katar!\n",i);
+            }
+            else
+            {
+                katar_index = size_per_proc * i +katar_locations[i];
+                printf("Process %d: Katar is found at index of %d\n",i,katar_index);
+            }
         }
     }
 
@@ -78,16 +86,13 @@ char *read_input(char *path)
 
 int search_katar(char *str, char *katar)
 {
-    int str_length, katar_length;
     int str_iterator, katar_iterator, last_index;
     int is_search_started = 0; //0: false, 1: true
 
-    str_length = strlen(str);
-    katar_length = strlen(katar);
     last_index = 0;
     katar_iterator = 0;
 
-    for (str_iterator = 0; str_iterator < str_length; str_iterator++)
+    for (str_iterator = 0; str_iterator < INPUT_LENGTH; str_iterator++)
     {
         if (is_search_started == 1)
         {
@@ -97,7 +102,7 @@ int search_katar(char *str, char *katar)
                 katar_iterator++;
 
                 // Searching successfully completed, return the index of the start index of the katar
-                if (katar_iterator == katar_length)
+                if (katar_iterator == KATAR_LENGTH)
                 {
                     return last_index;
                 }
@@ -119,7 +124,7 @@ int search_katar(char *str, char *katar)
             katar_iterator++;
             last_index = str_iterator;
         }
-        else if (str_iterator >= (str_length - katar_length - 1))
+        else if (str_iterator >= (INPUT_LENGTH - KATAR_LENGTH - 1))
         {
             //There is no need to continue for searching
             //Because the number of the rest of the characters is less than size of katar
@@ -182,29 +187,4 @@ void partition_input(char *input, char *katar, char *local_input, int p, int my_
         MPI_Recv(local_input,partition_size,MPI_CHAR,0,0,MPI_COMM_WORLD,&status);
     }
     
-}
-
-void Show_local_inputs(int p, int my_rank,char *local_input)
-{
-    MPI_Status status;
-    char *buf;
-    int source;
-    int dest = 0;
-    int tag = 0;
-    int size_per_proc = (int)INPUT_LENGTH / p;
-    int partition_size = size_per_proc + KATAR_LENGTH - 1;
-    buf =(char*) malloc(sizeof(char)*partition_size);
-
-    if(my_rank == 0)
-    {
-        printf("Process %d : %s\n",my_rank,local_input);
-        for(source=1; source<p; source++)
-        {
-            MPI_Recv(buf,partition_size,MPI_CHAR,source,tag,MPI_COMM_WORLD,&status);
-            printf("Process %d: %s\n",source, buf);
-        }
-        return;
-    }
-
-    MPI_Send(local_input,1,MPI_INT,dest,tag,MPI_COMM_WORLD);
 }
